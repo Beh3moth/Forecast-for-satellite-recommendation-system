b34d8c4f21ddf657d2a48c881edde8040e2a13ca
Nicole Perrotta <perrottanicole00@gmail.com>
2023-05-18 10:49:54 +0200

    get_weather_forecast updated

--- interface.py
+++ interface.py
@@ -13,12 +13,26 @@ class Interface:
     # There could be also the need of some kind of check on the aoi (it could be empty ecc).
     # this method is static because it doesn't modify any instance or class attributes.
     def get_weather_forecast(aoi, queue):
-        # conversion in json?
-        # json_aoi = json.loads(aoi)
+        # testing with a file
+        with open('AOIs\#0001_AoiID_1.geojson') as file:
+            data = json.load(file)
+
+        # actual aoi
+        features = data['features']
+        for feature in features:
+            geometry = feature['geometry']
+            aoiM = geometry['coordinates']
+
+        # conversion from Multipolygon to Polygon
+        aoi = aoiM[0]
+
+        # conversion in json
+        json_aoi = json.loads(aoi)
+
         # Check on the aoi?
 
         # Create the controller
         controller = Controller()
 
         # Call the method of the controller that takes the aoi as input and returns the final output.
-        controller.start_processing(aoi, queue)
+        return controller.start_processing(aoi, queue)
