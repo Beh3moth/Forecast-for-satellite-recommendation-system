8540358784ec400b2dec746d65bb6f31197aea6f
Beh3moth <75836092+Beh3moth@users.noreply.github.com>
2023-05-22 17:34:25 +0200

    Implemented working granularity adaptability

--- geoHashConverter.py
+++ geoHashConverter.py
@@ -1,14 +1,15 @@
 import geohash
 import numpy as np
 import json
-from shapely.geometry import multipolygon
-import geopandas
+from itertools import chain
 
 
 class GeoHashConverter:
 
     update_hours_interval = 0
     geo_hash_dim = 0
+    lat_step = 0.1
+    lon_step = 0.1
 
     def __init__(self):
         config_file = open('config.json')
@@ -26,36 +27,53 @@ class GeoHashConverter:
     # granularity of the geohash to be chosen, according to the maximum limit of API calls per day, then sets the
     # geo-hash-dim attribute to that chosen granularity. The higher the geohash granularity, the bigger the number of
     # geo hashes for a given AOI, the bigger the amount of API calls to be made
-    def set_geohash_granularity(self, polygon_geom: multipolygon):  # TODO: check how to handle a multipolygon as input
+    def set_geohash_granularity(self, set_polygon):  # TODO: check how to handle a multipolygon as input
 
-        rectangle = geopandas.GeoSeries(geopandas.GeoSeries(polygon_geom).envelope)
+        aoi_area_size = 0
 
-        # rectangle.to_crs({'init': 'epsg: 3857'})  # needed to convert to a Cartesian system projection
-        aoi_area_size = rectangle.area / 10 ** 6  # obtain the area in square kms
+        for polygon in set_polygon:
+            if polygon.area:
+                aoi_area_size += polygon.area
+
+        aoi_area_size = aoi_area_size * 111.4 * 111.13
+
+        # print("The area is: " + str(aoi_area_size))
 
         # Granularity (width, height) in kilometers
         hash_sizes = [(0, 0), (5000000, 5000000), (1250000, 6250000), (156000, 156000), (39100, 19500), (4890, 4890),
-                     (1220, 610), (153, 153), (38.2, 19.1), (4.77, 4.77), (1.19, 0.59)]
+                      (1220, 610), (153, 153), (38.2, 19.1), (4.77, 4.77), (1.19, 0.59)]
 
         hash_area_widths = []
 
         for pair in hash_sizes:
-            hash_area_widths.append(pair[0] * pair[1])
+            hash_area_widths.append((pair[0] * pair[1])/10 ** 6)
+
+        # print("areas: " + str(hash_area_widths))
 
         # First try with the 6th granularity
-        step = 6
+        step = 9
         amount_of_geohash = aoi_area_size / hash_area_widths[step]
 
-        tot_calls = self.compute_total_calls_per_day(amount_of_geohash)
+        tot_calls = self.compute_total_calls_per_day(int(amount_of_geohash))
+        # print("The number of total calls is:" + str(tot_calls))
         # Keep trying more coarse-grained geohash sizes until the amount of total calls is below 10000
 
         while tot_calls >= 10000 and step > 0:
+            # print('iteration')
             step -= 1
+            # print("iteration step: " + str(step))
             amount_of_geohash = aoi_area_size / hash_area_widths[step]
-            tot_calls = self.compute_total_calls_per_day(amount_of_geohash)
+            tot_calls = self.compute_total_calls_per_day(int(amount_of_geohash))
+
+        # print("The amount of geohash is: " + str(amount_of_geohash))
+        # print("step: " + str(step))
 
         # set chosen granularity to the geo_hash_dim attribute
         self.geo_hash_dim = step
+        self.lat_step = hash_sizes[step][0]/110.574/10 ** 3
+        self.lon_step = hash_sizes[step][1]/111.320/10 ** 3
+        # print("lat dim: ", self.lat_step)
+        # print("lon dim: ", self.lon_step)
 
     # TODO: To be changed according to the new approach :  "constructing the geoHash missing strings"
     def convert_polygon_to_geohash(self, multi_polygon):
@@ -74,10 +92,11 @@ class GeoHashConverter:
 
                 geohash_list = set()
 
-                for lat in np.arange(lat_min, lat_max, 0.1):
-                    for lon in np.arange(lon_min, lon_max, + 0.1):
+                for lat in np.arange(lat_min, lat_max, self.lat_step/2):
+                    for lon in np.arange(lon_min, lon_max, self.lon_step/2):
                         geohash_value = geohash.encode(lat, lon, self.geo_hash_dim)
                         geohash_list.add(geohash_value)
+
                 super_set.append(geohash_list)
 
         return super_set
