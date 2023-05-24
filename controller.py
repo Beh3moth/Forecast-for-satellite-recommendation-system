c508e6fbb66b14893b9228763fc106df7f6c7362
Nicole Perrotta <perrottanicole00@gmail.com>
2023-05-24 15:04:47 +0200

    modify_parameter(parameter_list) added

--- controller.py
+++ controller.py
@@ -1,12 +1,20 @@
 from geoHashConverter import GeoHashConverter
+import json
 
+def modify_parameter(parameter_list):
+    with open("config_parameter.json") as json_file:
+        data = json.load(json_file)
+    data['weatherParameters'] = parameter_list
+    updated_json = json.dumps(data, indent=4)
+    with open('config_parameter.json', 'w') as file:
+        file.write(updated_json)
 
 class Controller:
     def __init__(self):
         pass
 
     @staticmethod
-    def start_processing(polygon_geom, queue, info):
+    def start_processing(polygon_geom, queue, info, parameter_list):
         # the first thing to do is to convert the AOI in a geoHash string
         geohash_converter = GeoHashConverter()
 
@@ -14,3 +22,4 @@ class Controller:
         list_list_geohash = geohash_converter.convert_polygon_to_geohash(polygon_geom)
         queue.put(list_list_geohash)
         queue.put(info)
+        modify_parameter(parameter_list)
